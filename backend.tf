beb76100012586e551aba32402f428f1f34a8541
narendiransankar <narendiransankar1699@gmail.com>
2025-07-24 20:26:24 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket       = "aws-state-s3"
+    bucket       = "new-state-temp"
     key          = "hcl-usecase-9/terraform.tfstate"
     #profile      = "devops"
     region       = "ap-south-1"
     encrypt      = true
-    use_lockfile = true
+    use_lockfile = false
   }
 }
