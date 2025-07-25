82fe936063e181d0e297dbdd44753c0f7d83d572
narendiransankar <narendiransankar1699@gmail.com>
2025-07-25 07:51:14 +0530

    Update main.tf

--- modules/terraform-aws-helm/main.tf
+++ modules/terraform-aws-helm/main.tf
@@ -1,4 +1,15 @@
-
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.9"
+    }
+  }
+}
 
 # Install AWS Load Balancer Controller using HELM
 
